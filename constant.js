537e264ae885b4edefbaa40a5dceb9cd8be1f787
sophia banda <sophia.m.banda@gmail.com>
2019-07-21 18:59:49 -0400

    all the changes

---
+++ constant.js
@@ -0,0 +1,20 @@
+const _1999 = `I was dreamin' when I wrote this, so sue me if I go too fast\n
+But life is just a party and _______ _____ __ ____\n
+War is all around us, my mind says prepare to fight\n
+So if I gotta die I'm gonna listen to my body tonight\n`
+
+const Bohemian = `I see a little silhouetto of a man\n
+Scaramouch, Scaramouch will you do the Fandango\n
+__________ ___ __________ very very frightening me\n
+Gallileo, Gallileo, Gallileo, Gallileo, Gallileo, figaro, magnifico\n`
+
+const What_I_Got = `Early in the morning, risin' to the street\n
+_____ __ __ ____ _________ and I strap shoes on my feet\n
+Got to find a reason, a reason things went wrong\n
+Got to find a reason why my money's all gone\n
+I got a dalmatian, and I can still get high\n
+I can play the guitar like a motherfucking riot\n`
+
+module.exports._1999 = _1999;
+module.exports.Bohemian = Bohemian;
+module.exports.What_I_Got = What_I_Got;
---
+++ utils.js
@@ -0,0 +1,2 @@
+const randomizeQuestions = array => array.sort(() => Math.random() - 0.5);
+module.exports.randomizeQuestions = randomizeQuestions
