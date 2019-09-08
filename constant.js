7fe88536d1b667da3a9c5e42a98b51001482dfa9
sophia banda <sophia.m.banda@gmail.com>
2019-09-07 22:33:37 -0400

    added more songs

--- constant.js
+++ constant.js
@@ -15,6 +15,23 @@ Got to find a reason why my money's all gone\n
 I got a dalmatian, and I can still get high\n
 I can play the guitar like a motherfucking riot\n`
 
+const I_Get_Around = `Back to get wreck\n
+All respect to those who break they neck to keep their hoes in check\n
+'Cause oh they sweat a brotha majorly\n
+And I don't know why your girl keeps paging me\n
+She tells me that she needs me, cries when she leaves me\n
+And every time she sees me she squeeze, lady take it easy\n
+Hate to sound sleazy but tease me, _ _____ ____ __ __ ___ ____ ____.\n`
+
+const Juicy = `It was all a dream, I used to read Word Up! magazine\n
+Salt-n-Pepa and Heavy D up in the limousine\n
+Hangin' pictures on my wall\n
+Every Saturday Rap Attack, Mr. Magic, Marley Marl\n
+_ ___ __ ____ ____ ___ __ ____ ___\n
+Smokin' weed in Bambu, sippin' on Private Stock\n`
+
 module.exports._1999 = _1999;
 module.exports.Bohemian = Bohemian;
 module.exports.What_I_Got = What_I_Got;
+module.exports.I_Get_Around = I_Get_Around
+module.exports.Juicy = Juicy
